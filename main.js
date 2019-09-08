0e92d8c712d3829f1d62de86436ffb2ef171ff19
aleion <alejandro.ion.carvajal@gmail.com>
2019-09-08 09:27:10 -0300

    footer for web done

--- main.js
+++ main.js
@@ -2,15 +2,9 @@ const burgerBtn = () =>{
     const burger = document.querySelector('.toggler');
     const navList = document.querySelector('.nav-list');
     const navBar = document.querySelector('.navbar');
-    const burgerL1 = document.querySelector('.line1'); 
-    const burgerL2 = document.querySelector('.line2');
-    const burgerL3 = document.querySelector('.line3');
 
     burger.addEventListener('click', ()=>{
         navList.classList.toggle('nav-list-shown');
-        burgerL1.classList.toggle('line-1');
-        burgerL2.classList.toggle('line-2');
-        burgerL3.classList.toggle('line-3');
     });
 }
 
